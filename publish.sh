bf8afcec94246039fb747f8726407a968ca2e337
Chris Hartjes <chartjes@littlehart.net>
2016-11-09 17:32:15 -0500

    Added updated publish.sh script that uses the new SSL-compatible site URL

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@
 # was downloaded and placed in this directory instead of sculpin having been
 # installed globally.
 
-sculpin.phar generate --env=prod --url=https://littlehart.net/atthekeyboard
+sculpin.phar generate --env=prod --url=https://www.littlehart.net/atthekeyboard
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
 rsync -avze 'ssh -p 22' output_prod/ chartjes@littlehart.net:atthekeyboard/
